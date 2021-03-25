05e8b53e24799c9f38c01870829510cedbc8cb84
Gabriel Jeantet <gabriel.jeantetpro@gmail.com>
2021-03-25 15:19:12 +0100

    gitu commit

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-initdb -D $HOME/postgre/data
+# initdb -D $HOME/postgre/data
 ln -s $DIR/www $HOME/www
 ln -s $DIR/config/lighttpd.conf $HOME/.local/etc/lighttpd/lighttpd.conf
 
